1032483c3242f4fe1d36261d9ff5cb65ad3fc653
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-04-29 22:37:43 +0300

    feat: add update uri command to modify existing JSON record entries

---
+++ cmd/update_uri.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func UpdateUri() *cobra.Command {
+
+	updateCmd := &cobra.Command{
+		Use:   "update",
+		Short: "update uri data",
+	}
+	updateCommand := &cobra.Command{
+		Use:   "uri",
+		Short: "Update uri data",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			nameFile := readLine("Please enter name of file " + "... ")
+			name := readLine("Please enter name of " + "... ")
+			uri := readLine("Please add uri of " + name + "... ")
+			status := readLine("Please enter status of " + name + "... ")
+
+			file, err := os.ReadFile("json/" + nameFile + ".json")
+			if err != nil {
+				return err
+			}
+			var data []AddURIStruct
+			err = json.Unmarshal(file, &data)
+
+			if err != nil {
+				return err
+			}
+			for i := range data {
+				if name != "" {
+					data[i].Name = name
+				}
+				if uri != "" {
+					data[i].Uri = uri
+				}
+				if status != "" {
+					data[i].Status = status
+				}
+			}
+			jsonFile, err := json.MarshalIndent(data, "", "  ")
+			if err != nil {
+				return err
+			}
+			os.WriteFile("json/"+nameFile+".json", jsonFile, 0644)
+			fmt.Println("data updated successfully")
+			return nil
+		},
+	}
+
+	updateCmd.AddCommand(updateCommand)
+
+	return updateCmd
+}
+
+func init() {
+	rootCmd.AddCommand(UpdateUri())
+}
