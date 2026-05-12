832b764524d469d0313ac463ca2cf19991981d7a
Mohamed El-Refai <elrefai99@gmail.com>
2026-05-12 16:11:48 +0300

    docs: update CLI description for root command

--- cmd/root.go
+++ cmd/root.go
@@ -8,8 +8,8 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "app",
-	Short: "short description",
-	Long:  "long description of your CLI",
+	Short: "This CLI tool is designed to simplify the process of exporting MongoDB data to JSON files.\n It allows you to save your database structures and content in a format that is easy to backup, share, and restore.",
+	Long:  "This CLI tool is designed to simplify the process of exporting MongoDB data to JSON files.\n It allows you to save your database structures and content in a format that is easy to backup, share, and restore.",
 }
 
 func Execute() {
