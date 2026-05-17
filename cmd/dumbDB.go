0e93f92f5be145fa72df1f9f5196e321e5a69d9d
Mohamed El-Refai <elrefai99@gmail.com>
2026-05-17 09:39:19 +0300

    feat: update database export path to include timestamped subdirectories and ensure directory creation

--- cmd/dumbDB.go
+++ cmd/dumbDB.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"time"
 
 	"dumber/internal/dumb"
 
@@ -34,8 +35,8 @@ func DumbDB() *cobra.Command {
 			if err != nil {
 				return err
 			}
-			out := filepath.Join(homeDir, "Downloads", "dumber-export")
-
+			out := filepath.Join(homeDir, "Downloads", "backup", "dumber-export", time.Now().Format("2006-01-02"))
+			os.Mkdir(out, 0755)
 			for _, data := range dbSettings {
 				dumb.Dumb(data.Name, out, data.Uri, true)
 
